5f60888d953ae785957668ffb850a8a3da28e429
Max Mamis <maxmamis@gmail.com>
2014-03-16 16:58:57 -0400

    IIFE, convert to spaces

--- build/js/editor.js
+++ build/js/editor.js
@@ -1,19 +1,23 @@
+(function () {
+
 function getClipOrder() {
-	var clips = [];
-	$( ".timeline li.active" ).each(function() {
-	 	clips.push( $(this).data("clip") );
-	});
-	return clips;
+    var clips = [];
+    $( ".timeline li.active" ).each(function() {
+        clips.push( $(this).data("clip") );
+    });
+    return clips;
 }
 
 function playNewSequence() {
-	console.log( " ");
-	console.log("clips " , getClipOrder());
-	MC.player.load( {
-		clips: getClipOrder()
-	});
+    console.log( " ");
+    console.log("clips " , getClipOrder());
+    MC.player.load( {
+        clips: getClipOrder()
+    });
 }
 
 $('#sortable').on( "sortstop", function( event, ui ) {
-	playNewSequence();
-} );
+    playNewSequence();
+});
+
+}());
